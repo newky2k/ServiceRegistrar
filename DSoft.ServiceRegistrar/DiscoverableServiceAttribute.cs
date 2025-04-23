2406fe574f905fe374703f64db8711377f5f2fc2
David Humphreys <newky2k@users.noreply.github.com>
2025-04-23 19:47:21 +0100

    Added documentation, signing and new test harness

--- DSoft.ServiceRegistrar/DiscoverableServiceAttribute.cs
+++ DSoft.ServiceRegistrar/DiscoverableServiceAttribute.cs
@@ -4,19 +4,38 @@ using System.Text;
 
 namespace DSoft.ServiceRegistrar
 {
+    /// <summary>
+    /// Discoverable Service Attribute.
+    /// Implements the <see cref="Attribute" />
+    /// </summary>
+    /// <seealso cref="Attribute" />
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     public class DiscoverableServiceAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the interface.
+        /// </summary>
+        /// <value>The interface.</value>
+        public Type Interface { get; private set; }
 
+        /// <summary>
+        /// Gets the implementation.
+        /// </summary>
+        /// <value>The implementation.</value>
+        public Type Implementation { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiscoverableServiceAttribute"/> class.
+        /// </summary>
+        /// <param name="interfaceType">Type of the interface.</param>
+        /// <param name="implementationType">Type of the implementation.</param>
         public DiscoverableServiceAttribute(Type interfaceType, Type implementationType)
         {
             Interface = interfaceType;
             Implementation = implementationType;
         }
 
-        public Type Interface { get; private set; }
 
-        public Type Implementation { get; private set; }
     }
 
 }
